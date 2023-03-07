171a2494bdf173ce10592350dde381076e24545f
siddhantmendhe <38434303+siddhantmendhe@users.noreply.github.com>
2023-03-07 17:56:41 +0530

    making car title bold while typing in form

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -2,9 +2,16 @@ import { useSelector,useDispatch } from "react-redux";
 import {removeCar} from '../store'
 function CarList(){
     const dispatch=useDispatch();
-    const cars= useSelector(({cars:{data, searchTerm}})=>{
-        return data.filter((car)=>  car.name.toLowerCase().includes(searchTerm.toLowerCase())
+    const {name, cars}= useSelector(({form,cars:{data, searchTerm}})=>{
+        const filteredCars=data.filter((car)=>  car.name.toLowerCase().includes(searchTerm.toLowerCase())
         );
+
+        return{
+            cars:filteredCars,
+            name:form.name
+        }
+
+
     })
 
     const handleDelete=(car)=>{
@@ -13,8 +20,10 @@ function CarList(){
     }
 
     const renderedCars=cars.map((car)=>{
+        //Should car be BOLD
+        const bold=name&&car.name.toLowerCase().includes(name.toLowerCase());
         return(
-            <div key={car.id} className="panel">
+            <div key={car.id} className={`panel ${bold && 'bold'}`}>
                 <p>
                     {car.name} - ${car.cost}
                 </p>
